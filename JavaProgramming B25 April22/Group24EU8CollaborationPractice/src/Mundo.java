3d21013cf300b70cad20d6f35240bdf093704657
mariajoana <gokalp.maryam@gmail.com>
2022-04-23 19:29:05 +0100

    2rd commit

--- JavaProgramming B25 April22/Group24EU8CollaborationPractice/src/Mundo.java
+++ JavaProgramming B25 April22/Group24EU8CollaborationPractice/src/Mundo.java
@@ -1,6 +1,15 @@
 public class Mundo {
-    /*
+    public static void main(String[] args) {
 
+        System.out.println("Hello everyone");
+        System.out.println("Testing");
+        System.out.println("Lets do it");
+        System.out.println("The game started");
+    }
+    /*
     Isto foi criado pelo o prefessor
      */
+
+
+
 }
