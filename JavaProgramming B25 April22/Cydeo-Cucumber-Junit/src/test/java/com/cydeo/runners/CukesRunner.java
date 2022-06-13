9b73ac88356133c778023933c2165e46d22c4220
mariajoana <gokalp.maryam@gmail.com>
2022-06-13 12:21:21 +0100

    runners:CukesRunner

--- JavaProgramming B25 April22/Cydeo-Cucumber-Junit/src/test/java/com/cydeo/runners/CukesRunner.java
+++ JavaProgramming B25 April22/Cydeo-Cucumber-Junit/src/test/java/com/cydeo/runners/CukesRunner.java
@@ -10,7 +10,8 @@ import org.junit.runner.RunWith;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions"
+        glue = "com/cydeo/step_definitions",
+        dryRun = false
 
 
 )
