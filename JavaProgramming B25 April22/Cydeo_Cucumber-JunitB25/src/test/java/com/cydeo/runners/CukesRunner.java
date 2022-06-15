b03b823050cefc75c9779872f41bff8ab6463b64
mariajoana <gokalp.maryam@gmail.com>
2022-06-15 18:19:43 +0100

    primeiro

---
+++ JavaProgramming B25 April22/Cydeo_Cucumber-JunitB25/src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/cydeo/stepDefinitions",
+        dryRun = true
+
+)
+public class CukesRunner {
+
+}
+
+
+//Glue Provide The steps definition package location, where The selenium implementation code will be writing
+//Open Web False to close--> dryRun = true
+// true means that do not run my step definitions but only check if any step is missing snippet.
